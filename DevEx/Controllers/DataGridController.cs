5e8ec4afef9a3f6a7b6a89222c53cc62f5b52400
Luke Hammer <luke@lukehammermagic.com>
2018-02-13 13:57:21 -0800

    Added Files

---
+++ DevEx/Controllers/DataGridController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DevEx.Controllers
+{
+    public class DataGridController : Controller
+    {
+        public ActionResult RowEditingAndEditingEvents()
+        {
+            return View();
+        }
+    }
+}
---
+++ DevEx/Models/DataGrid/InMemoryEmployeesDataContext.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevEx.Models.DataGrid
+{
+    public class InMemoryEmployeesDataContext
+    {
+        const string SessionKey = "3fd387f5-610a-4060-9204-619ef6e6f3ef";
+
+        public ICollection<Employee> Employees
+        {
+            get
+            {
+                var session = HttpContext.Current.Session;
+                if (session[SessionKey] == null)
+                    session[SessionKey] = SampleData.DataGridEmployees;
+
+                return (ICollection<Employee>)session[SessionKey];
+            }
+        }
+
+        public void SaveChanges()
+        {
+            foreach (var employee in Employees.Where(a => a.ID == 0))
+            {
+                employee.ID = Employees.Max(a => a.ID) + 1;
+            }
+        }
+
+
+
+    }
+
+
+        }
+    }
